405028e89b232586c4b201a32f4632a6969bd48c
haseenask8739 <haseenask.sp@gmail.com>
2024-10-03 18:06:03 +0200

    american guy commits

--- src/test/java/gitDevelop/newBranch.java
+++ src/test/java/gitDevelop/newBranch.java
@@ -57,7 +57,7 @@ package gitDevelop;
 		statusCode(200)
 		.body("date.id[0]",equalTo(7));
 	}
-	void Test_03() {
+	void Test_04() {
 		//Response responce=RestAssured.get("https://reqres.in/api/users?page=2");
 			Response responce=get("https://reqres.in/api/users?page=2");
 			System.out.println("this is my first code with git");
@@ -81,6 +81,16 @@ package gitDevelop;
 	System.out.println("this is my first code with git4");
 	System.out.println("this is my first code with git5");
 	System.out.println("this is my first code with git6");
+	}
+	void Test_05() {
+		System.out.println("this is my first code with git7");
+		System.out.println("this is my first code with git8");
+		System.out.println("this is my first code with git9");
+		given()
+		.get("https://reqres.in/api/users?page=2")
+		.then().
+		statusCode(200)
+		.body("date.id[0]",equalTo(7));
 	}
 	}
 
